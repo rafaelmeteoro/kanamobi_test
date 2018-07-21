887a09fbbeaf56f69ea1b7d7f439cead40d878e3
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-07-21 19:51:30 -0300

    create layout to ui repositories

--- app/src/main/java/com/meteoro/kanamobitest/application/MyApplication.kt
+++ app/src/main/java/com/meteoro/kanamobitest/application/MyApplication.kt
@@ -1,10 +1,22 @@
 package com.meteoro.kanamobitest.application
 
 import android.app.Application
+import com.meteoro.kanamobitest.core.di.component.DaggerLibraryComponent
+import com.meteoro.kanamobitest.core.di.component.LibraryComponent
+import com.meteoro.kanamobitest.core.di.module.ApplicationModule
+import com.meteoro.kanamobitest.core.di.module.LibraryModule
 
 class MyApplication : Application() {
 
+    companion object {
+        lateinit var component: LibraryComponent
+    }
+
     override fun onCreate() {
         super.onCreate()
+        component = DaggerLibraryComponent.builder()
+                .applicationModule(ApplicationModule(this))
+                .libraryModule(LibraryModule(this))
+                .build()
     }
 }
---
+++ app/src/main/java/com/meteoro/kanamobitest/ui/repositories/RepositoriesActivity.kt
@@ -0,0 +1,13 @@
+package com.meteoro.kanamobitest.ui.repositories
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.meteoro.kanamobitest.R
+
+class RepositoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repositories)
+    }
+}
